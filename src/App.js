0ef433139fe506376e32ec23bff8c6e01d685a3b
DILLIP KUMAR PUTI <putikishore63@gmail.com>
2024-02-02 21:25:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  const data2 = [
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" },
+  ];
+
+  useEffect(() => {
+    setData(data2);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const sortByDate = () => {
+    data2.sort((a, b) => new Date(b.date) - new Date(a.date));
+    setData(data2);
+  };
+
+  const sortByViews = () => {
+    data2.sort((a, b) => b.views - a.views);
+    setData(data2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
